234401ccdf04b56eaf49061122e9c313ba7a61c9
Jack Franklin <jack@jackfranklin.net>
2017-03-13 21:40:37 +0000

    Kill the comments

--- src/Cards.elm
+++ src/Cards.elm
@@ -5,9 +5,6 @@ import Sets
 import Properties
 
 
--- 20 money cards, including 6 1M cards, 5 2M cards, 3 3M cards, 3 4M cards, 2 5M cards and 1 10M card.
-
-
 allCards : List Card
 allCards =
     List.concat
@@ -48,7 +45,3 @@ moneyCards : List Card
 moneyCards =
     [ ( 6, 1 ), ( 5, 2 ), ( 3, 3 ), ( 3, 4 ), ( 2, 5 ), ( 1, 10 ) ]
         |> List.concatMap makeCardsFromTuple
-
-
-
--- 47 action cards (can use as money cards), including 10 color rent cards, 3 any rent cards, 2 Deal Breakers, 3 Forced Deal cards, 3 Sly Deal cards, 3 Just Say No Cards, 3 Debt Collectors, 3 It's My Birthday cards, 2 Double the Rent cards, 3 houses, 2 hotels and 10 Pass Go cards.
